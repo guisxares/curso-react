20ee4dcec898d22eec7d0463428b9b163a0972ee
guisoaresz <guisxares@hotmail.com>
2025-05-24 01:04:40 -0300

    aplicação finalizada

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import Input from "./Input";
+
+function AddTask({ addTaskClick }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <div className="space-y-4 p-6 flex flex-col bg-slate-200 rounded-md shadow">
+      <Input
+        type="text"
+        placeholder="Digite o título da tarefa"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <Input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          if (!title.trim() || !description.trim())
+            return alert("Preencha os campos corretamente!");
+          addTaskClick(title, description);
+          setTitle("");
+          setDescription("");
+        }}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+      >
+        Adicionar
+      </button>
+    </div>
+  );
+}
+
+export default AddTask;
